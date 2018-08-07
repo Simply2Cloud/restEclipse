931401a88a45bdc2e9299ba727bfa1df481b5601
Simply2Cloud <41632445+Simply2Cloud@users.noreply.github.com>
2018-08-07 13:04:20 -0400

    View and Save (all, add2)
    View and Save working
    Modifying issures

--- restful-webservices/restful-webservices/src/main/java/com/cloudComputing/rest/webservices/restfulwebservices/user/Lead.java
+++ restful-webservices/restful-webservices/src/main/java/com/cloudComputing/rest/webservices/restfulwebservices/user/Lead.java
@@ -2,41 +2,63 @@ package com.cloudComputing.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+
 
 
 
 @Entity
+@Table(name = "lead")
 public class Lead {
 
 	@Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id;
-	private int phone;
-	private String name;
-	private String email;
-	private String profession;
-	private String leadType;
-	private String leadSource;
-	private String leadDate;
-	private String leadTime;
-	private String leadStatus;
-	private String comments;
-	private String referenceName;
-	public int getId() {
+	@SequenceGenerator(name = "my_seq", sequenceName = "seq1", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "my_seq")
+	@Column(name = "Id")
+	private Integer id = 0;
+	@Column(name = "Phone")
+	private Integer phone = 0;
+	@Column(name = "Name")
+	private String name = "";
+	@Column(name = "Email")
+	private String email = "";
+	@Column(name = "Profession")
+	private String profession = "";
+	@Column(name = "LeadType")
+	private String leadType = "";
+	@Column(name = "LeadSource")
+	private String leadSource = "";
+	@Column(name = "LeadDate")
+	private String leadDate = "";
+	@Column(name = "LeadTime")
+	private String leadTime = "";
+	@Column(name = "LeadStatus")
+	private String leadStatus = "";
+	@Column(name = "Comments")
+	private String comments = "";
+	@Column(name = "ReferenceName")
+	private String referenceName = "";
+
+	
+	
+	public Integer getId() {
 		return id;
 	}
-	public void setId(int id2) {
+	public void setId(Integer id2) {
 		this.id = id2;
 	}
 	public Integer getPhone() {
 		return phone;
 	}
-	public void setPhone(Integer phone) {
-		this.phone = phone;
+	public void setPhone(Integer i) {
+		this.phone = i;
 	}
 	public String getName() {
 		return name;
@@ -106,7 +128,7 @@ public class Lead {
 				+ referenceName + "]";
 	}
 	
-	public Lead(int i) {
+	public Lead(Integer i) {
 		super();
 		this.id = i;
 	}
--- restful-webservices/restful-webservices/src/main/java/com/cloudComputing/rest/webservices/restfulwebservices/user/LeadRep.java
+++ restful-webservices/restful-webservices/src/main/java/com/cloudComputing/rest/webservices/restfulwebservices/user/LeadRep.java
@@ -1,7 +1,12 @@
 package com.cloudComputing.rest.webservices.restfulwebservices.user;
 
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.cloudComputing.rest.webservices.restfulwebservices.user.Lead;
@@ -9,7 +14,8 @@ import com.cloudComputing.rest.webservices.restfulwebservices.user.Lead;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 @Repository
-public interface LeadRep extends CrudRepository<Lead, Long> {
-	
-
+public interface LeadRep extends CrudRepository<Lead, String> {
+	 @Modifying(clearAutomatically = true)
+	 @Query(value = "insert into lead(ID,Phone,Name) values (3,001,joey)", nativeQuery = true)  
+	 List<Lead> setTestID(@Param("id") String id);
 }
